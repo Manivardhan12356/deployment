ce29f98ff3e27e42c5b603fb903d48aba19d0b94
Mani vardhan <manivardhan12356@gmail.com>
2023-06-12 17:40:32 +0530

    second  commit

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const admin = require("firebase-admin");
 const path = require('path')
+const PORT = process.env.PORT
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -33,10 +34,10 @@ app.post("/", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-   res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+   res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
 })
 
-app.listen(process.env.URL || 8000, () => {
+app.listen(PORT || 8000, () => {
    console.log(`Server started on port 8000`);
 });
 
